3f8ee0b288b43b138735ee2754b388b8bb1e3f6b
Christian Lüdemann <christian@christianlydemann.com>
2020-04-26 09:36:01 +0200

    test: mocking sections endpoint

--- apps/course-client-e2e/src/support/page-objects/course.po.ts
+++ apps/course-client-e2e/src/support/page-objects/course.po.ts
@@ -1,6 +1,11 @@
 export class CoursePage {
   static goToPage() {
     cy.visit('/course');
+    cy.server();
+    cy.route(
+      'http://localhost:3333/api/sections',
+      'fixture:course-sections.json'
+    );
   }
 
   static seeLesson() {
