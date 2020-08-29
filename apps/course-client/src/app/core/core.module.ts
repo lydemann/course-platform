b4182335a798ebecb617eb90b76af90389d6e89e
Christian Lüdemann <christian@christianlydemann.com>
2020-08-29 19:03:56 +0200

    chore: setup angular material

---
+++ apps/course-client/src/app/core/core.module.ts
@@ -0,0 +1,31 @@
+import { HttpClient } from '@angular/common/http';
+import { NgModule, Optional, SkipSelf } from '@angular/core';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http, `/assets/i18n/`, '.json');
+}
+
+@NgModule({
+  imports: [
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    })
+  ]
+})
+export class CoreModule {
+  constructor(
+    @Optional()
+    @SkipSelf()
+    parentModule: CoreModule
+  ) {
+    if (parentModule) {
+      throw new Error('CoreModule is already loaded. Import only in AppModule');
+    }
+  }
+}
--- apps/course-client/src/app/shared/shared.module.ts
+++ apps/course-client/src/app/shared/shared.module.ts
@@ -1,12 +1,24 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
+
 import { FeatureToggleModule } from '@course-platform/shared/util/util-feature-toggle';
+import { AppMaterialModule } from './material.module';
+
+// TODO: organize imports here
+const imports = [];
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, RouterModule, FeatureToggleModule],
-  exports: [CommonModule, RouterModule, FeatureToggleModule],
+  imports: [CommonModule, RouterModule, FeatureToggleModule, TranslateModule],
+  exports: [
+    CommonModule,
+    RouterModule,
+    FeatureToggleModule,
+    AppMaterialModule,
+    TranslateModule
+  ],
   providers: []
 })
 export class SharedModule {}
