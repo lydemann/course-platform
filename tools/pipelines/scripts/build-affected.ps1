b4ba2da8a5a8fb9dbca4a0158e4263b0b1ed6b21
Christian Lüdemann <christian@christianlydemann.com>
2020-09-20 14:37:07 +0200

    Fix build all (#8)

--- tools/pipelines/scripts/build-affected.ps1
+++ tools/pipelines/scripts/build-affected.ps1
@@ -3,7 +3,7 @@ $AffectedAppsString = $AffectedAppsObj[4];
 
 if (!$AffectedAppsString -and $AffectedAppsString -eq "") {
     Write-Host "No affected apps. Building all apps.";
-    Invoke-Expression 'npm run affected:build -- --base=origin/master --head=HEAD --prod --plain --all';
+    Invoke-Expression 'npm run affected:build -- --prod --plain --all';
     return;
 }
 
