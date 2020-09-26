2129811aeb1a20f677d1a914a4dee43d5701f00e
Christian Lüdemann <christian@christianlydemann.com>
2020-09-26 12:30:20 +0200

    test: fix e3e

--- apps/course-client-e2e/src/integration/app.spec.ts
+++ apps/course-client-e2e/src/integration/app.spec.ts
@@ -1,9 +1,11 @@
 import { CoursePage } from '../support/page-objects/course.po';
+import { LoginPage } from '../support/page-objects/login.po';
 
 describe('course-client', () => {
   beforeEach(() => CoursePage.goToPage());
 
   it('should show lesson', () => {
+    LoginPage.login();
     CoursePage.seeLesson();
   });
 });
