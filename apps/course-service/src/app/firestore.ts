0cbdee5718165366f095e689ee61902692c69f23
Christian Lüdemann <christian@christianlydemann.com>
2020-10-22 07:26:04 +0200

    chore: remove firebase config

--- apps/course-service/src/app/firestore.ts
+++ apps/course-service/src/app/firestore.ts
@@ -1,8 +1,4 @@
 import * as admin from 'firebase-admin';
 
-const serviceAccount = require('../../serviceAccountKey.json');
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
-});
+admin.initializeApp();
 export const firestoreDB = admin.firestore();
