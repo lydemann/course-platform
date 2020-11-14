53987f205e430cd00c6b8621c1acd4766a6b6e36
Christian Lüdemann <christian@christianlydemann.com>
2020-11-14 09:42:51 +0100

    fix: auth

--- apps/course-admin/src/app/shared/shared.module.ts
+++ apps/course-admin/src/app/shared/shared.module.ts
@@ -1,13 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { SharedUiModule } from '@course-platform/shared/ui';
 import { AppMaterialModule } from './material.module';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, AppMaterialModule, SharedUiModule],
-  exports: [CommonModule, AppMaterialModule, SharedUiModule],
+  imports: [CommonModule, AppMaterialModule, SharedUiModule, FlexLayoutModule],
+  exports: [CommonModule, AppMaterialModule, SharedUiModule, FlexLayoutModule],
   providers: []
 })
 export class SharedModule {}
