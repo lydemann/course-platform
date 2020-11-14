268d3b4e344f785382672e115ba6aa5ea77a3808
Christian Lüdemann <christian@christianlydemann.com>
2020-11-14 16:17:02 +0100

    chore: remove unused

--- libs/shared/data-access/src/resources/graphql.module.ts
+++ libs/shared/data-access/src/resources/graphql.module.ts
@@ -1,15 +1,7 @@
-import { HttpHeaders } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import {
-  ApolloClientOptions,
-  ApolloLink,
-  createHttpLink,
-  InMemoryCache
-} from '@apollo/client/core';
+import { ApolloClientOptions, InMemoryCache } from '@apollo/client/core';
 import { Apollo, APOLLO_OPTIONS } from 'apollo-angular';
 import { HttpLink } from 'apollo-angular/http';
-import { setContext } from 'apollo-link-context';
-import firebase from 'firebase';
 
 import { Endpoints, ENDPOINTS_TOKEN } from './endpoints';
 
@@ -32,23 +24,4 @@ export function createApollo(
     }
   ]
 })
-export class GraphQLModule {
-  /**
-   *
-   */
-  constructor(private apollo: Apollo, private httpLink: HttpLink) {
-    this.init();
-  }
-
-  async init() {
-    // const idToken = await firebase.auth().currentUser.getIdToken();
-    // // const httpLink = createHttpLink({ uri: this.endpoints.courseServiceUrl });
-    // const authLink = setContext((operation, context) => ({
-    //   headers: {
-    //     Authorization: `Bearer ${idToken}`
-    //   }
-    // })) as any;
-    // const link = ApolloLink.from([authLink]);
-    // this.apollo.client.setLink(link);
-  }
-}
+export class GraphQLModule {}
