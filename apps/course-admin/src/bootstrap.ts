d15183c537d905d6e2dc9e200e11c53fee8dc4a2
Christian Lüdemann <christian@christianlydemann.com>
2023-02-02 13:22:15 +0700

    fix: course admin

---
+++ apps/course-admin/src/bootstrap.ts
@@ -0,0 +1,35 @@
+import { enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
+
+declare global {
+  interface Window {
+    config: any;
+  }
+}
+
+if (environment.production) {
+  enableProdMode();
+}
+
+// load app config
+const xhttp = new XMLHttpRequest();
+xhttp.open('GET', 'assets/app-config.json', true);
+xhttp.onreadystatechange = function () {
+  if (this.readyState === 4 && this.status === 200) {
+    const config = JSON.parse(this.responseText);
+    window.config = config;
+
+    if (environment.production) {
+      enableProdMode();
+    }
+
+    platformBrowserDynamic()
+      .bootstrapModule(AppModule)
+      // eslint-disable-next-line no-console
+      .catch((err) => console.error(err));
+  }
+};
+xhttp.send();
