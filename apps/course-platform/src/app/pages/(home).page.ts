6b346d7cc7d8af52d9138d80623592c5efd2068a
Christian Lüdemann <christian@christianlydemann.com>
2023-12-20 14:34:26 -0300

    feat: added analog app

---
+++ apps/course-platform/src/app/pages/(home).page.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'course-platform-home',
+  standalone: true,
+  imports: [],
+  template: ` <h3>Welcome to course platform</h3> `,
+})
+export default class HomeComponent {}
