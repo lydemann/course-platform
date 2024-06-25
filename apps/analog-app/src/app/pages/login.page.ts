2e407ccb8f7757afb59864de6fed673b8e9ad56e
Christian Lüdemann <christian@christianlydemann.com>
2024-06-25 17:38:31 +0200

    chore: run build again

--- apps/analog-app/src/app/pages/login.page.ts
+++ apps/analog-app/src/app/pages/login.page.ts
@@ -1,7 +1,7 @@
 /* eslint-disable @nx/enforce-module-boundaries */
 import { RouteMeta } from '@analogjs/router';
 import { LoginComponent } from '@course-platform/course-client/feature';
-
+//
 export const routeMeta: RouteMeta = {
   title: 'Login',
   providers: [],
