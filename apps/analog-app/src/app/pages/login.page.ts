248e7fa090473e71cdc325af608e105c4f67864a
Christian Lüdemann <christian@christianlydemann.com>
2024-06-25 16:56:07 +0200

    chore: remove guard

--- apps/analog-app/src/app/pages/login.page.ts
+++ apps/analog-app/src/app/pages/login.page.ts
@@ -1,11 +1,9 @@
 /* eslint-disable @nx/enforce-module-boundaries */
 import { RouteMeta } from '@analogjs/router';
 import { LoginComponent } from '@course-platform/course-client/feature';
-import { redirectIfLoggedInServerGuard } from '@course-platform/course-client/shared/domain';
 
 export const routeMeta: RouteMeta = {
   title: 'Login',
-  canActivate: [redirectIfLoggedInServerGuard],
   providers: [],
 };
 
