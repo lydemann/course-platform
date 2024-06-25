abb2f4756e77fa60cab30b7f922c8fa0c811e310
Christian Lüdemann <christian@christianlydemann.com>
2024-06-25 17:01:21 +0200

    chore: fix

--- apps/course-platform-analog/vite.config.ts
+++ apps/course-platform-analog/vite.config.ts
@@ -4,14 +4,12 @@ import analog from '@analogjs/platform';
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 import { typescriptPaths } from 'rollup-plugin-typescript-paths';
 import { defineConfig, splitVendorChunkPlugin } from 'vite';
-import { cpSync } from 'node:fs';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   return {
     root: __dirname,
     cacheDir: `../../node_modules/.vite`,
-    publicDir: 'src/public',
     build: {
       target: ['es2020'],
       outDir: '../../dist/./course-platform-analog/client',
