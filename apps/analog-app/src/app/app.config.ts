dbc7ca91e2049a53a2f3b782682509f20a2795f2
Christian Lüdemann <christian@christianlydemann.com>
2024-06-26 15:00:25 +0200

    chore: add to analog app

--- apps/analog-app/src/app/app.config.ts
+++ apps/analog-app/src/app/app.config.ts
@@ -1,13 +1,130 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideHttpClient, withFetch } from '@angular/common/http';
-import { provideClientHydration } from '@angular/platform-browser';
 import { provideFileRouter } from '@analogjs/router';
+import {
+  HttpClient,
+  provideHttpClient,
+  withFetch,
+  withInterceptors,
+} from '@angular/common/http';
+import {
+  APP_INITIALIZER,
+  ApplicationConfig,
+  ErrorHandler,
+  importProvidersFrom,
+} from '@angular/core';
+import {
+  BrowserModule,
+  provideClientHydration,
+  withHttpTransferCacheOptions,
+} from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import * as Sentry from '@sentry/angular-ivy';
+import {
+  CoreModule,
+  CourseClientDomainModule,
+  environment,
+} from '@course-platform/course-client/shared/domain';
+import {
+  authInterceptor,
+  authServerInterceptor,
+} from '@course-platform/shared/auth/domain';
+import { ENDPOINTS_TOKEN, Endpoints } from '@course-platform/shared/domain';
+import { NgrxUniversalRehydrateBrowserModule } from '@course-platform/shared/ngrx-universal-rehydrate';
+import { cookieInterceptor } from '@course-platform/shared/ssr/domain';
+import { FeatureToggleService } from '@course-platform/shared/util/util-feature-toggle';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { Router } from '@angular/router';
+
+export function preloadFeagureFlags(
+  featureToggleService: FeatureToggleService
+) {
+  return () => featureToggleService.getFeatureFlags().toPromise();
+}
+
+export function endpointsFactory() {
+  return {
+    courseServiceUrl: environment.courseServiceUrl,
+  } as Endpoints;
+}
+
+export function httpLoaderFactory(http: HttpClient) {
+  const host =
+    import.meta.env['VITE_ANALOG_PUBLIC_BASE_URL'] || 'http://localhost:4200';
+  return new TranslateHttpLoader(http, `${host}/assets/i18n/`, '.json');
+}
+
+Sentry.init({
+  dsn: 'https://b1eed35b84c2dab0896baa461082a81b@o4507049117548544.ingest.us.sentry.io/4507049118859264',
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration({
+      maskAllText: false,
+      blockAllMedia: false,
+    }),
+  ],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: [
+    'localhost',
+    /^https:\/\/app.christianlydemann\.com/,
+  ],
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }),
     provideFileRouter(),
-    provideClientHydration(),
-    provideHttpClient(withFetch()),
+    // makes sure the client is hydrated with the server state to avoid redundant client requests
+    provideClientHydration(
+      withHttpTransferCacheOptions({
+        includePostRequests: true,
+      })
+    ),
+    provideHttpClient(
+      withFetch(),
+      withInterceptors([
+        cookieInterceptor,
+        authServerInterceptor,
+        authInterceptor,
+      ])
+    ),
+    {
+      provide: ENDPOINTS_TOKEN,
+      useFactory: endpointsFactory,
+    },
+    {
+      provide: Sentry.TraceService,
+      deps: [Router],
+    },
+    {
+      provide: APP_INITIALIZER,
+      useFactory: () => () => {},
+      deps: [Sentry.TraceService],
+      multi: true,
+    },
+    {
+      provide: ErrorHandler,
+      useValue: Sentry.createErrorHandler({
+        showDialog: environment.production,
+        logErrors: true,
+      }),
+    },
+    importProvidersFrom([
+      BrowserModule,
+      BrowserAnimationsModule,
+      CoreModule,
+      CourseClientDomainModule,
+      TranslateModule.forRoot({
+        loader: {
+          provide: TranslateLoader,
+          useFactory: httpLoaderFactory,
+          deps: [HttpClient],
+        },
+      }),
+      NgrxUniversalRehydrateBrowserModule.forRoot({}),
+    ]),
   ],
 };
--- apps/analog-app/src/main.server.ts
+++ apps/analog-app/src/main.server.ts
@@ -1,10 +1,17 @@
-import 'zone.js/node';
+/// <reference types="vite/client" />
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable @nx/enforce-module-boundaries */
 import '@angular/platform-server/init';
+import 'zone.js/node';
 
 import { enableProdMode } from '@angular/core';
-import { renderApplication } from '@angular/platform-server';
 import { bootstrapApplication } from '@angular/platform-browser';
+import { renderApplication } from '@angular/platform-server';
+// eslint-disable-next-line @nx/enforce-module-boundaries
+import { REQUEST as SSR_REQUEST } from 'ngx-cookie-service-ssr';
+// eslint-disable-next-line @nx/enforce-module-boundaries
 
+import { ClientRequest, ServerResponse } from 'http';
 import { AppComponent } from './app/app.component';
 import { config } from './app/app.config.server';
 
@@ -14,10 +21,22 @@ if (import.meta.env.PROD) {
 
 const bootstrap = () => bootstrapApplication(AppComponent, config);
 
-export default async function render(url: string, document: string) {
+export default async function render(
+  url: string,
+  document: string,
+  { req, res }: { req: ClientRequest; res: ServerResponse }
+) {
   const html = await renderApplication(bootstrap, {
     document,
     url,
+    platformProviders: [
+      { provide: SSR_REQUEST, useValue: req },
+      { provide: 'RESPONSE', useValue: res },
+    ],
   });
+
+  res.setHeader('Cache-Control', 'public, max-age=600, s-maxage=1200');
+  res.setHeader('expires', '600');
+
   return html;
 }
