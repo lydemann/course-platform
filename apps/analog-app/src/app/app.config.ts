6ae4803b7c9111dcb87e5dd53d6b72e0e05c4777
Christian Lüdemann <christian@christianlydemann.com>
2024-06-26 15:48:09 +0200

    chore: add more

--- apps/analog-app/src/app/app.config.ts
+++ apps/analog-app/src/app/app.config.ts
@@ -3,7 +3,7 @@ import {
   importProvidersFrom,
   provideZoneChangeDetection,
 } from '@angular/core';
-import { provideHttpClient, withFetch } from '@angular/common/http';
+import { HttpClient, provideHttpClient, withFetch } from '@angular/common/http';
 import {
   BrowserModule,
   provideClientHydration,
@@ -13,8 +13,24 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {
   CoreModule,
   CourseClientDomainModule,
+  environment,
 } from '@course-platform/course-client/shared/domain';
 import { NgrxUniversalRehydrateBrowserModule } from '@course-platform/shared/ngrx-universal-rehydrate';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { ENDPOINTS_TOKEN, Endpoints } from '@course-platform/shared/domain';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+
+export function endpointsFactory() {
+  return {
+    courseServiceUrl: environment.courseServiceUrl,
+  } as Endpoints;
+}
+
+export function httpLoaderFactory(http: HttpClient) {
+  const host =
+    import.meta.env['VITE_ANALOG_PUBLIC_BASE_URL'] || 'http://localhost:4200';
+  return new TranslateHttpLoader(http, `${host}/assets/i18n/`, '.json');
+}
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -22,11 +38,22 @@ export const appConfig: ApplicationConfig = {
     provideFileRouter(),
     provideClientHydration(),
     provideHttpClient(withFetch()),
+    {
+      provide: ENDPOINTS_TOKEN,
+      useFactory: endpointsFactory,
+    },
     importProvidersFrom([
       BrowserModule,
       BrowserAnimationsModule,
       CoreModule,
       CourseClientDomainModule,
+      TranslateModule.forRoot({
+        loader: {
+          provide: TranslateLoader,
+          useFactory: httpLoaderFactory,
+          deps: [HttpClient],
+        },
+      }),
       NgrxUniversalRehydrateBrowserModule.forRoot({}),
     ]),
   ],
